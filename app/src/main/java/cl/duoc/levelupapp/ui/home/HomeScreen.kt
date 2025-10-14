a00180dc7a11320935111345ffc54f26bde7882b
Rodrigo Gallardo <167040497+ragallardom@users.noreply.github.com>
2025-10-14 02:28:10 -0300

    Fix compile errors in Carrito and ProductDetail

--- app/src/main/java/cl/duoc/levelupapp/ui/home/HomeScreen.kt
+++ app/src/main/java/cl/duoc/levelupapp/ui/home/HomeScreen.kt
@@ -1,16 +1,19 @@
 package cl.duoc.levelupapp.ui.home
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
